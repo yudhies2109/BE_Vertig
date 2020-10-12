e4283672ad56f2cd005399f0a87fd2c496a0e74f
yudhies2109 <ma.yudhistira@hotmail.com>
2020-10-12 15:42:42 +0700

    Read data restaurant

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,25 @@ var router = express.Router();
 module.exports = function (pool) {
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+// Get Data Restaurant
+
+router.get('/', function (req, res, next) {
+  try {
+    let sql = `SELECT * FROM restaurants `;
+    pool.query(sql, (err, data) => {
+      res.json({
+        success: true,
+        message: "Data has been load successfully",
+        data: data.rows
+      });
+    })
+  } catch (err) {
+    res.json({
+      error: true,
+      message: `something went wrong : ${err.message}`
+    })
+  }
 });
 
 return router;
