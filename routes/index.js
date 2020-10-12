049eb04ba968e151122b4d72704275fc68578697
yudhies2109 <ma.yudhistira@hotmail.com>
2020-10-12 21:43:06 +0700

    added router restaurant

--- routes/index.js
+++ routes/index.js
@@ -25,5 +25,32 @@ router.get('/', function (req, res, next) {
   }
 });
 
+// Added Data Restaurant
+router.post('/', function (req, res, next) {
+  try {
+    let namaRestaurant = req.body.nama_restaurant;
+    let almtRestaurant = req.body.almt_restaurant;
+    let tlpRestaurant = req.body.nmr_tlpn;
+    let sql = `INSERT INTO restaurants (nama_restaurant, almt_restaurant, nmr_tlpn) VALUES ('${namaRestaurant}', '${almtRestaurant}', '+${tlpRestaurant}')`;
+    pool.query(sql, (err) => {
+      res.json({
+        success: true,
+        message: "Data has been added successfully",
+        data: {
+          restaurant: namaRestaurant,
+          alamatRestaurant: almtRestaurant,
+          teleponRestaurant: tlpRestaurant 
+        }
+      });
+    })
+  } catch (err) {
+    res.json({
+      error: true,
+      message: `something went wrong : ${err.message}`
+    })
+  }
+});
+
+
 return router;
 }
