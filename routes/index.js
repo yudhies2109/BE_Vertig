4f844b52a15d158884ba8137f701b69792fc066b
yudhies2109 <ma.yudhistira@hotmail.com>
2020-10-13 06:08:59 +0700

    delete restaurant API

--- routes/index.js
+++ routes/index.js
@@ -51,6 +51,53 @@ router.post('/', function (req, res, next) {
   }
 });
 
+// Update Data Restaurant
+router.put('/:id', function (req, res, next) {
+  let id = req.params.id;
+
+  try {
+    let namaRestaurant = req.body.nama_restaurant;
+    let almtRestaurant = req.body.almt_restaurant;
+    let tlpRestaurant = req.body.nmr_tlpn;
+    let sql = `UPDATE restaurants SET nama_restaurant = '${namaRestaurant}', almt_restaurant = '${almtRestaurant}', nmr_tlpn = '${tlpRestaurant}' WHERE id_restaurant = ${id}`;
+    pool.query(sql, (err, data) => {
+      res.json({
+        success: true,
+        message: "Data has been updated",
+        data: {
+          restaurant: namaRestaurant,
+          alamatRestaurant: almtRestaurant,
+          teleponRestaurant: tlpRestaurant
+        }
+      });
+    })
+  } catch (err) {
+    res.json({
+      error: true,
+      message: `something went wrong : ${err.message}`
+    })
+  }
+});
+
+// Delete Restaurant
+router.delete('/:id', function (req, res, next) {
+  let id = req.params.id;
+
+  try {
+    let sql = `DELETE FROM restaurants WHERE id_restaurant = ${id}`;
+    pool.query(sql, (err, data) => {
+      res.json({
+        success: true,
+        message: "Data has been deleted"
+      });
+    })
+  } catch (err) {
+    res.json({
+      error: true,
+      message: `something went wrong : ${err.message}`
+    })
+  }
+});
 
 return router;
 }
